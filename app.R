67395a975324f1064b332ad4f2ace9c56fcb124f
fortune2rw <fortunehormos215@gmail.com>
2026-02-28 16:40:22 +0000

    Integrate ggplot2 for scatter plot visualization
    Added ggplot2 library and updated scatter plot implementation to use ggplot for better visualization. Adjusted various comments and modified the output of the per gene stats table.

--- app.R
+++ app.R
@@ -4,6 +4,7 @@
 
 library(shiny)
 library(dplyr)
+library(ggplot2)
 #------------------------- Boilerplate functions ------------------------------
 
 compute_gene_stats <- function( expr_mat, meta_df, target_ct){
@@ -29,7 +30,7 @@ compute_gene_stats <- function( expr_mat, meta_df, target_ct){
   )
 }
 
- ##select marker gene
+##select marker gene
 pick_marker_gene <- function(gene_stats_df){
   gene_stats_df <- gene_stats_df%>%
     arrange(desc(diff), desc(det_in))
@@ -50,13 +51,13 @@ expr_matrix <- read.csv("https://raw.githubusercontent.com/HackBio-Internship/20
 cell_metadata <- read.csv("https://raw.githubusercontent.com/HackBio-Internship/2025_project_collection/refs/heads/main/sc_synthetic/cell_metadata.csv", header = T)
 umap_coordinates <- read.csv("https://raw.githubusercontent.com/HackBio-Internship/2025_project_collection/refs/heads/main/sc_synthetic/umap_coordinates.csv", header = T)
 
- # Make sure IDs match and are aligned
+# Make sure IDs match and are aligned
 common_ids <- Reduce(
   intersect, 
   list(rownames(expr_matrix), cell_metadata$cell_id, umap_coordinates$cell_id)
 )
 
- #reorder metadata and UMAP to match expr-matrx rows
+#reorder metadata and UMAP to match expr-matrx rows
 cell_metadata <- cell_metadata[match(common_ids, cell_metadata$cell_id), ]
 umap_coordinates <- umap_coordinates[match(common_ids, umap_coordinates$cell_id),]
 
@@ -84,7 +85,7 @@ ui <- fluidPage(
                    icon = icon("image"))
     ),
     
-      mainPanel(
+    mainPanel(
       tableOutput(outputId = "per_gene_stats"),
       plotOutput(outputId = "scatter_plot")
     )
@@ -108,54 +109,62 @@ server <- function(input, output) {
   #per gene stats table
   output$per_gene_stats <- renderTable({
     stats_df <- stats_reactive()
-    head(stats_df[order(- stats_df$diff, - stats_df$det_in),], 20)
+    head(stats_df[order(- stats_df$diff, - stats_df$det_in),], 30)
   })
   
   #choose marker gene
   output$marker_gene <- renderText({
-  stats_df <- stats_reactive()
-  top_gene <- pick_marker_gene(stats_df)
-  diff_val <- stats_df$diff[stats_df$gene == top_gene]
-  paste("Cell type :", input$cell_type , "| Marker :", top_gene, "| Diff =", 
-        round(diff_val, 3))
-})
+    stats_df <- stats_reactive()
+    top_gene <- pick_marker_gene(stats_df)
+    diff_val <- stats_df$diff[stats_df$gene == top_gene]
+    paste("Cell type :", input$cell_type , "| Marker :", top_gene, "| Diff =", 
+          round(diff_val, 3))
+  })
   
   #  scatter plot 
-data_plot <- eventReactive(input$buttons, {
-  stats_df <- stats_reactive()
-  marker_gene <- pick_marker_gene(stats_df) #get marker gene from stats table
-  
-  marker_expr <- expr_matrix[, marker_gene] #get per-cell expression 
-  
-  marker_expre_scaled <- scale_0_100(marker_expr) #scale 0 - 100
-  plot_df$marker_expression <- marker_expre_scaled
-  
-  list(
-    x = plot_df[,"UMAP_1"],
-    y = plot_df[,"UMAP_2"],
-    cell_type = input$cell_type,
-    marker_gene = marker_gene,
-    marker_expr = plot_df$marker_expression
-  )
-})
-
-
-  output$scatter_plot <- renderPlot({
-    pts <- data_plot()
-    col_scale <- colorRampPalette(c("lightgrey","darkred"))(100)[
-      cut(pts$marker_expr, breaks = 100, include.lowest = T)
-    ]
+  data_to_plot <- reactive({
+    stats_df <- stats_reactive()
+    marker_gene <- pick_marker_gene(stats_df) #get marker gene
+    marker_expr <- expr_matrix[, marker_gene] # get per cell expression
+    marker_expr_scaled <- scale_0_100(marker_expr)
     
-    plot(x = pts$x,
-         y = pts$y,
-         col = col_scale,
-         ylab = "UMAP_2",
-         xlab = "UMAP_1",
-         bg = as.factor(cell_metadata$cell_type),
-         pch = 21,
-         cex = 1.3,
-         main = paste("UMAP: ", pts$marker_gene))
+    data.frame(
+      "x_value" = plot_df$UMAP_1,
+      "y_value" = plot_df$UMAP_2,
+      "col_value" = marker_expr_scaled,
+      "top_gene" = marker_gene
+    )
   })
+  
+  output$scatter_plot <- renderPlot({
+    
+    #draw scatter plot
+    if(input$cell_type == "All"){
+      ggplot(plot_df, aes(x = UMAP_1, y = UMAP_2, colour = cell_type))+
+        geom_point(size = 2.5, alpha = 1)+
+        labs( title = "Scatter Plot Coloured by Cell Type",
+              color = "Cell Type")+
+        theme_classic()+
+        theme(title = element_text(size = 15, face = "bold"))
+    }else{
+      df <- data_to_plot()
+      
+      #create scatter plot by top maker gene expression in all cell
+      ggplot(df, aes(x = x_value, y = y_value, colour = col_value ))+
+        geom_point(size = 2.5)+
+        scale_color_gradient2(low = "blue",mid ="white", high = "red")+
+        labs(
+          x = "UMAP_1",
+          y = "UMAP_2",
+          title = paste("Scatter Plot :", df$top_gene, "Across All Cell Type"),
+          color = "Expression"
+        )+
+        theme_classic()+
+        theme(title = element_text(size = 15, face = "bold"))
+    }
+  })
+  
+  
 }
 
 
